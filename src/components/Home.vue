aeff85967771c904e9d0c3f346f1b4fcd5ff7f8a
iobtl <tedmundhtl@gmail.com>
2020-06-22 13:53:39 +0800

    Finished form front-end transition

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <h1 class="text-5xl font-bold font-sans text-white ">
+      Vue Trello
+    </h1>
     <login-form />
   </div>
 </template>
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,112 +1,114 @@
 <template>
   <div class="container mx-auto w-full max-w-xs">
-    <form
-      class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-      v-if="registered"
-      @submit.prevent="loginUser()"
-    >
-      <div class="mb-4">
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2 text-left text-lg"
-          for="username"
-          >Username</label
-        >
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          v-model="username"
-          placeholder="enter your username"
-        />
-      </div>
-      <div class="mb-6">
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2 text-left text-lg"
-          for="password"
-          >Password</label
-        >
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="password"
-          v-model="password"
-          placeholder="enter your password"
-        />
-      </div>
-      <div class="flex items-center justify-between">
-        <button
-          class="bg-vue bg-opacity-85 hover:bg-green-700 text-white font-bold rounded px-4 py-2"
-          type="submit"
-          @click="$emit('set-message')"
-        >
-          Login
-        </button>
-        <button
-          class="bg-vue bg-opacity-85 hover:bg-green-700 text-white font-bold rounded px-4 py-2"
-          @click="switchRegister"
-        >
-          Sign up
-        </button>
-      </div>
-    </form>
-    <form
-      v-else
-      @submit.prevent="
-        registerUser();
-        switchRegister();
-      "
-    >
-      <div class="mb-4">
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2 text-left text-lg"
-          for="username"
-          >Username</label
-        >
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          v-model="username"
-          placeholder="enter your username"
-        />
-      </div>
-      <div class="mb-6">
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2 text-left text-lg"
-          for="password"
-          >Password</label
-        >
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="password"
-          v-model="password"
-          placeholder="enter your password"
-        />
-      </div>
-      <div class="mb-6">
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2 text-left
+    <transition name="slide-fade" mode="out-in">
+      <form
+        key="loginPage"
+        v-if="registered"
+        class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        @submit.prevent="loginUser()"
+      >
+        <div class="mb-4">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2 text-left text-lg"
+            for="username"
+            >Username</label
+          >
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            v-model="username"
+            placeholder="enter your username"
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2 text-left text-lg"
+            for="password"
+            >Password</label
+          >
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="password"
+            v-model="password"
+            placeholder="enter your password"
+          />
+        </div>
+        <div class="flex items-center justify-between">
+          <button
+            class="bg-vue bg-opacity-85 hover:bg-green-700 text-white font-bold rounded px-4 py-2"
+            type="submit"
+            @click="$emit('set-message')"
+          >
+            Login
+          </button>
+          <button
+            class="bg-vue bg-opacity-85 hover:bg-green-700 text-white font-bold rounded px-4 py-2"
+            @click="switchRegister"
+          >
+            Sign up
+          </button>
+        </div>
+      </form>
+      <form
+        key="registerPage"
+        v-else
+        class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        @submit.prevent="registerUser()"
+      >
+        <div class="mb-4">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2 text-left text-lg"
+            for="username"
+            >Username</label
+          >
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            v-model="username"
+            placeholder="enter your username"
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2 text-left text-lg"
+            for="password"
+            >Password</label
+          >
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="password"
+            v-model="password"
+            placeholder="enter your password"
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2 text-left
         text-lg"
-          for="email"
-          >Email</label
-        >
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          v-model="email"
-          placeholder="enter your email"
-        />
-      </div>
-      <div class="flex item-center justify-between">
-        <button
-          class="bg-vue bg-opacity-85 hover:bg-green-700 text-white font-bold rounded px-4 py-2 mb-4"
-          type="submit"
-          @click="$emit('set-message')"
-        >
-          Register
-        </button>
-        <button
-          class="bg-gray-600 bg-opacity-85 hover:bg-gray-700 text-white font-bold rounded px-4 py-2 mb-4"
-          @click="switchRegister"
-        >
-          Back
-        </button>
-      </div>
-    </form>
+            for="email"
+            >Email</label
+          >
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            v-model="email"
+            placeholder="enter your email"
+          />
+        </div>
+        <div class="flex item-center justify-between">
+          <button
+            class="bg-vue bg-opacity-85 hover:bg-green-700 text-white font-bold rounded px-4 py-2 mb-4"
+            type="submit"
+            @click="$emit('set-message')"
+          >
+            Register
+          </button>
+          <button
+            class="bg-gray-600 bg-opacity-85 hover:bg-gray-700 text-white font-bold rounded px-4 py-2 mb-4"
+            @click="switchRegister"
+          >
+            Back
+          </button>
+        </div>
+      </form>
+    </transition>
   </div>
 </template>
 
@@ -144,3 +146,17 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateY(10px);
+  opacity: 0;
+}
+</style>
