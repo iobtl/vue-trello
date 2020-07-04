b97b743e13f98ab9252a5221440b7a6b69f91220
iobtl <tedmundhtl@gmail.com>
2020-07-04 20:13:47 +0800

    Basic list creation

--- src/components/BoardDetails.vue
+++ src/components/BoardDetails.vue
@@ -4,17 +4,24 @@
       <h1>{{$route.params.name}}</h1>
     </div>
     <div
-      class="bg-gray-600 hover:bg-gray-700 bg-opacity-50 pointer-cursor container rounded cursor-pointer text-center text-base h-8 w-48 px-2 py-1 text-white font-normal"
+      v-if="!formCreation"
+      class="bg-gray-600 hover:bg-gray-700 bg-opacity-50 pointer-cursor container rounded cursor-pointer text-center text-base h-8 w-56 px-2 py-1 text-white font-normal"
+      @click="triggerCreation"
     >Create new list</div>
-    <form @submit.prevent="createList">
-      <span class>
-        <input v-model="listName" placeholder="Enter list name" />
+    <form v-else @submit.prevent="createList" class="container mx-left w-56 bg-white">
+      <span>
+        <input
+          class="shadow appearance-none border rounded w-48 m-2 py-2 px-3 text-gray-700 text-sm leading-tight focus:outline-none focus:shadow-outline"
+          v-model="listName"
+          placeholder="Enter list name"
+        />
       </span>
       <div>
         <button
-          class="bg-gray-600 bg-opacity-50 align-left w-1/8 mt-2 bg-opacity-85 hover:bg-gray-700 text-white font-bold font-sans rounded px-4 py-2"
+          class="bg-vue hover:bg-green-700 align-left w-1/8 text-white font-bold font-sans rounded px-4 py-2 mx-2 mb-2"
           type="submit"
         >Create</button>
+        <span class="text-xl ml-4 cursor-pointer" @click="triggerCreation">X</span>
       </div>
     </form>
   </div>
@@ -23,12 +30,16 @@
 export default {
   data() {
     return {
-      listName: ""
+      listName: "",
+      formCreation: false
     };
   },
   methods: {
     createList() {
       this.listName = "";
+    },
+    triggerCreation() {
+      this.formCreation = !this.formCreation;
     }
   }
 };
