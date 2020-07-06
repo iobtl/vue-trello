37974c4c596f129aa1658c69b17c85a7138dd9de
iobtl <tedmundhtl@gmail.com>
2020-07-06 19:51:47 +0800

    Router redirect if accesing user page without login

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,34 @@ import BoardDetails from '../components/BoardDetails.vue';
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
   routes: [
-    { path: '/', component: Home },
-    { path: '/:user', component: Boards },
-    { path: '/:user/boards/:name', component: BoardDetails },
+    { path: '/', name: 'Login', component: Home },
+    {
+      path: '/:user',
+      name: 'UserBoards',
+      component: Boards,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/:user/boards/:name',
+      name: 'Board',
+      component: BoardDetails,
+      meta: { requiresAuth: true },
+    },
   ],
   mode: 'history',
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // Login required
+    if (!window.localStorage.getItem('loggedInUser')) {
+      next({ name: 'Login' });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
